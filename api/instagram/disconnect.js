0dd68694ec01a3de5746660509b4da4a86d43b9d
Elon-Yomtovyan <elonyomtovyan@gmail.com>
2026-07-17 14:45:28 +0300

    Expose Instagram connection API on Vercel

---
+++ api/instagram/disconnect.js
@@ -0,0 +1,2 @@
+import{setCookie}from'../_instagram.js';
+export default async function handler(req,res){if(req.method!=='POST')return res.status(405).json({error:'Method not allowed'});res.setHeader('Set-Cookie',setCookie('scs_instagram','',0));return res.status(204).end()}
