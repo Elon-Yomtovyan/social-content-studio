012085807806b313e90d09e4e151915468e770ee
Elon-Yomtovyan <elonyomtovyan@gmail.com>
2026-07-17 18:46:58 +0300

    Remove superseded api/facebook/disconnect.js

--- api/facebook/disconnect.js
+++
@@ -1,2 +0,0 @@
-import { setCookie } from "../_facebook.js";
-export default async function handler(req, res) { if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" }); res.setHeader("Set-Cookie", setCookie("scs_facebook", "", 0)); return res.status(204).end(); }
